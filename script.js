ea08a615d6c25eb186e81f9b7a0ef1679131a77f
przemyslawkrezel123 <przemyslawkrezel123@gmail.com>
2025-10-20 19:29:47 +0000

    1_1.2

--- script.js
+++ script.js
@@ -1,25 +1,4 @@
 (function () {
   
-  document.getElementById('ex3_one').addEventListener('dragover', function(e) {
-    e.preventDefault();
-    return false;
-  });
   
-  document.getElementById('ex3_two').addEventListener('dragover', function(e) {
-    e.preventDefault();
-    return false;
-  });
-  
-  document.getElementById('ex3_two').addEventListener('drop', function(e) {
-    e.preventDefault();
-    this.appendChild(element);
-    return false;
-  });
-  
-  document.getElementById('ex3_one').addEventListener('drop', function(e) {
-    e.preventDefault();
-    this.appendChild(element);
-    return false;
-  });
-
 })();
